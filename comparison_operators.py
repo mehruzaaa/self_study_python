9007074c3f0b5de024b8982fb0d3ee61ba977182
mehruzaaa <mehruzap@icloud.com>
2026-07-09 17:33:22 +0500

    Add files via upload

---
+++ comparison_operators.py
@@ -0,0 +1,32 @@
+#  COMPARISON OPERATORS
+#  It compares two values and return True or False based on the rusult
+#  == equal to
+#  != not equal
+#  < less than
+#  <= leess than or equal
+#  > greater than
+#  >= greater than or equal
+
+print(10 == 10)
+print(10 != 10)
+print(7 > 3)
+print(7 >= 3)
+print(3 < 7)
+print(7 <= 7)
+
+
+#  strings can be compared too! you can compare strings too alphabetically, not  just numbers
+print("a" < "b")
+#  Python is case-sensitive, so "a" and "A" are treated as different values
+print("a" == "A")
+#  = assigns, ==compares
+
+#  chained comparison, check multiple conditions in one line, just like in math
+#  it evaluates it from left to right, checking each condition one by one
+print(1 < 4 < 6)
+
+age = 20
+print(18 <= age <= 30)
+
+
+
