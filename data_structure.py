e0db6c51453b901ad0bd79fa07692a3b80b7d5c9
mehruzaaa <mehruzap@icloud.com>
2026-07-10 22:08:37 +0500

    Add files via upload

---
+++ data_structure.py
@@ -0,0 +1,31 @@
+#  DATA STRUCTURE
+#  is a way of organizing and storing data so it can be used efficiently
+#  there are 4types: list[], tuple(), set{}, dict{}
+
+#  functions vs methods
+#  function:                  methods:
+#  costs = [10, 20, 15]       costs = [10, 20, 15]
+#  len(costs0)                costs.remove(10)
+
+
+
+#  List:::
+["a", "b", "c", "c"]
+#  ordered collection of items
+#  changeable
+#  allows duplicates
+
+#  will learn:
+#  ho to create list?
+#  how to acces and read list?
+#  how to unpack list?
+#  how to explore and analyze list?
+#  how to chane list?
+#  how to order list?
+#  how to copy list?
+#  how to combine?
+#  how to iterate?
+#  how to transform?
+#  how to filter?
+#  list comprehensions
+
