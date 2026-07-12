2e9d9df8a552afa358c915acde81ea749edd2c6f
mehruzaaa <mehruzap@icloud.com>
2026-07-12 21:23:04 +0500

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,35 @@
+#  PYTHON TUPLES
+
+my_list = [10, 30, 20, 10]
+print(my_list)  # ordered  # allow duplicates
+print(my_list[1])  # indexed
+
+my_list[3] = 40
+print(my_list)  # mutable
+
+
+
+
+#  Data structure tuple:::
+#  tuple - ordered collection that can't be changed after creating it
+
+my_tuple = (10, 30, 20, 10)
+print(my_tuple)   # ordered   # allow duplicates
+print(my_tuple[1])  # indexed
+#  is not mutable!
+
+print(sorted(my_tuple))  #  the result will be a list
+
+
+#  use case of tuple - protect your values
+
+
+  
+
+
+
+
+
+
+
+
