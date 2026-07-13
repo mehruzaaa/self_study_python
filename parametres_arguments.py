be80cd34eae9f8d7d9c7f4f6503e5d6e0e37d2e7
mehruzaaa <mehruzap@icloud.com>
2026-07-13 16:17:11 +0500

    Add files via upload

---
+++ parametres_arguments.py
@@ -0,0 +1,26 @@
+#  PYTHON PARAMETRES VS ARGUMENTS
+
+#  Function shapes:::
+
+#  Parametres vs Arguments
+#  parameters - Names used in function definition that describe what data the functionn expects
+#  arguments - actual values passed in a function call that are assigned to parameters
+
+
+#  rule: normalize strings
+#  remove extra spaces and make text lowercase
+def clean_name(name):
+    print(name.strip().lower())
+clean_name(" MariA  ")
+clean_name("KUMAR ")
+#  hardcoded value issue:   it is not reusable because it always cleans the same value
+#  pass the value as a parameter to handle any input
+#  works with any value:  values change, but the logic stays the same
+
+
+
+
+
+
+
+
