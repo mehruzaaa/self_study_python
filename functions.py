f70542043ab15f7a8a108333dc586c30cf500c4b
mehruzaaa <mehruzap@icloud.com>
2026-07-13 15:51:01 +0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,36 @@
+#  PYTHON FUNCTIONS
+#  Function - a small, reusable block of code that does one specific job
+
+def make_coffee():
+    print("Start Machine")
+    print("Make Coffee")
+    print("Add Milk")
+    print("Enjoy it")
+
+print("Wake up")
+make_coffee()
+print("Working for a while")
+make_coffee()
+make_coffee()
+
+
+
+
+import math
+
+# build-in function(just calling)
+print(len("Python"))
+
+# function from libraries (import then call)
+number = 4.2
+print(math.ceil(number))
+
+# user defined function (define then call)
+def greet():
+    print("Hello")
+greet()
+
+
+
+
+
